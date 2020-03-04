53e54b880d16a45da3247e50b10deea0c15c3ddf
Anton Katekov <void0main@gmail.com>
2020-03-04 21:56:33 +0900

    Add file manager

---
+++ CrashpadMac/FileManager.h
@@ -0,0 +1,17 @@
+//
+//  FileManager.h
+//  CrashpadMac
+//
+//  Created by Anton Katekov on 2020/03/04.
+//  Copyright © 2020 Anton Katekov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FileManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CrashpadMac/FileManager.m
@@ -0,0 +1,13 @@
+//
+//  FileManager.m
+//  CrashpadMac
+//
+//  Created by Anton Katekov on 2020/03/04.
+//  Copyright © 2020 Anton Katekov. All rights reserved.
+//
+
+#import "FileManager.h"
+
+@implementation FileManager
+
+@end
